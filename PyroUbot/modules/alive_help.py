ebc3de163e9a6865da679a93708fef0b4c402030
seobangbob <seobangbob@gmail.com>
2025-11-07 13:26:51 +0700

    Update alive_help.py

--- PyroUbot/modules/alive_help.py
+++ PyroUbot/modules/alive_help.py
@@ -156,7 +156,7 @@ async def user_help(client, message):
 @PY.INLINE("^user_help")
 async def user_help_inline(client, inline_query):
     SH = await ubot.get_prefix(inline_query.from_user.id)
-    msg = f"<blockquote><b>✮ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a>\nᚗ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs: {len(HELP_COMMANDS)}\nᚗ ᴘʀᴇꜰɪx: {' '.join(SH)}\n♅ ᴜsᴇʀʙᴏᴛ ʙʏ : @FizzNescafe</b></blockquote>"
+    msg = f"<blockquote><b>✮ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a>\nᚗ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs: {len(HELP_COMMANDS)}\nᚗ ᴘʀᴇꜰɪx: {' '.join(SH)}\n♅ ᴜsᴇʀʙᴏᴛ ʙʏ : @Jaxeltelegram</b></blockquote>"
     results = [InlineQueryResultArticle(
         title="Help Menu!",
         reply_markup=InlineKeyboardMarkup(paginate_modules(0, HELP_COMMANDS, "help")),
@@ -181,7 +181,7 @@ async def help_callback(client, callback_query):
     tutup_match = re.match(r"help_tutup\((.+?)\)", callback_query.data)
     back_match = re.match(r"help_back", callback_query.data)
     SH = await ubot.get_prefix(callback_query.from_user.id)
-    top_text = f"<blockquote><b>✮ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={callback_query.from_user.id}>{callback_query.from_user.first_name} {callback_query.from_user.last_name or ''}</a>\nᚗ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs: {len(HELP_COMMANDS)}\nᚗ ᴘʀᴇꜰɪx: {' '.join(SH)}\n♅ ᴜsᴇʀʙᴏᴛ ʙʏ : @FizzNescafe</b></blockquote>"
+    top_text = f"<blockquote><b>✮ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={callback_query.from_user.id}>{callback_query.from_user.first_name} {callback_query.from_user.last_name or ''}</a>\nᚗ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs: {len(HELP_COMMANDS)}\nᚗ ᴘʀᴇꜰɪx: {' '.join(SH)}\n♅ ᴜsᴇʀʙᴏᴛ ʙʏ : @Jaxeltelegram</b></blockquote>"
 
     if mod_match:
         module = (mod_match.group(1)).replace(" ", "_")
